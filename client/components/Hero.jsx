5eddcbd89c677b09038facc57580a6feda02ebc2
Evans Ding <evansding@gmail.com>
2021-07-28 21:45:59 -0700

    planning out individual heroes

---
+++ client/components/Hero.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+
+const Hero = (props) => {
+
+  const [hero, setHero] = useState(0);
+
+  useEffect(() => {
+    setHero(props.number)
+  })
+
+
+  let heroObject = {
+    0: 'something.svg',
+    1: '1.svg',
+    2: '2.svg',
+    3: '3.svg',
+    4: '4.svg',
+    5: '5.svg',
+    6: '6.svg',
+    7: '7.svg',
+    8: '8.svg',
+    9: '9.svg',
+    10: '2.svg'
+
+  }
+
+  // if(props.number === 1) {
+  //   hero = 'one jpg';
+  // }
+
+  return(
+    <div>{heroObject[hero]}</div>
+  )
+
+
+}
+
+
+export default Hero;
--- client/components/Summon.jsx
+++ client/components/Summon.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Hero from './Hero.jsx';
 
 
 const Summon = (props) => {
+
+  const [heroNumber, createHero] = useState(0);
+
+  let currentHero = '';
+
+  const handleClick = () => {
+    createHero(Math.round(Math.random() * 10));
+
+  }
+
+  if(heroNumber !== 0) {
+    let currentHero = 'notzero'
+  }
+
   return(
-    <div>this is summoning</div>
+    <>
+    <div>summon jpg</div>
+    <Hero number = {heroNumber}/>
+    <button onClick = {handleClick}>Summon</button>
+    </>
+
   )
 
 
